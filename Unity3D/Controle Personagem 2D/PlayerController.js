b0155b2dfc0974b48a457d3c51e50d0716c20144
CreativeBlaster <icaro-sol@hotmail.com>
2022-03-07 22:00:07 -0300

    Atualizando com meu primeiro codigo de animação 2D em Unity3D

---
+++ Unity3D/Controle Personagem 2D/PlayerController.js
@@ -0,0 +1,55 @@
+﻿#pragma strict
+
+var idleState : int = 0;
+
+function Start () {
+   
+    //imagem.GetComponent.<SpriteRenderer>().sortingOrder = 3;
+}
+
+function Update () {
+    var vertical = Input.GetAxis("Vertical");
+    var horizontal = Input.GetAxis("Horizontal");
+ 
+    if (vertical > 0)
+    {
+        Debug.Log("CIMA");
+       GetComponent.<Animator>().SetInteger("Direction", 2);
+      GetComponent.<Animator>().speed = 2;
+       
+       
+      
+    }
+    else if (vertical < 0)
+    {
+    	 Debug.Log("BAIXO");
+       GetComponent.<Animator>().SetInteger("Direction", 0);
+      GetComponent.<Animator>().speed = 2;
+      
+              
+    }
+    else if (horizontal > 0)
+    {
+    	Debug.Log("DIREITA");
+        GetComponent.<Animator>().SetInteger("Direction", 3);
+       GetComponent.<Animator>().speed = 2;
+       
+        
+        
+    }
+    else if (horizontal < 0)
+    {
+    	Debug.Log("ESQUERDA");
+        GetComponent.<Animator>().SetInteger("Direction", 1);
+        GetComponent.<Animator>().speed = 2;
+    	
+    	
+    }
+    else if(horizontal == 0 && vertical == 0){
+    GetComponent.<Animator>().speed = 0;
+   
+   
+    }
+}
+
+ 
